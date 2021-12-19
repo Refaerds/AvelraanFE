d48bc323cd68662959ba0d9bffe3d5f6a03c0b24
Maryna Iholnykova <maryna.iholnykova@ionos.com>
2021-12-19 11:52:38 +0200

    character component done

--- avelraan-fe/src/components/Character/Character.vue
+++ avelraan-fe/src/components/Character/Character.vue
@@ -1,24 +1,42 @@
 <template>
     <div class="m-3 d-flex flex-column align-items-center max-w-7">
         <div class="d-flex flex-row flex-nowrap">
-            <div>
+            <div :id="avatarId">
 <!--                    TODO: add router link-->
                 <b-avatar size="lg" rounded="lg"
+                          badge-variant="danger" badge-offset="-0.4rem"
                           :disabled="!char.IsAlive"
                           to="/"
                           @click="onCharacterClick"
                           :src="getImgUrl(char.Logbook.PortraitNr)"
-                ></b-avatar>
+
+                >
+                    <template v-if="char.InFight" #badge>
+                        <fight-icon class="av-icon"
+                                    :id="avatarId + '-fight'"
+                        ></fight-icon>
+                        <b-tooltip :target="avatarId + '-fight'" placement="right">
+                            {{ char.Name }} is in fight right now
+                        </b-tooltip>
+                    </template>
+                </b-avatar>
                 <div class="text-center mt-2 text-break">{{ char.Name }}</div>
             </div>
+
+<!--            Dead tooltip-->
+            <b-tooltip v-if="!char.IsAlive" :target="avatarId">
+                This warrior has met his glorious end
+            </b-tooltip>
+
             <div class="d-flex flex-column pl-2">
 <!--                    TODO: add router link-->
-                <b-link v-if="char.HasLevelup"
+<!--                Level up button-->
+                <b-link v-if="char.IsAlive && char.HasLevelup"
                         @mouseenter="hoverClass = 'av-icon-animated'"
                         @mouseleave="hoverClass = ''"
                         to="/"
                 >
-                    <UpgradeIcon :class="['av-icon', hoverClass]" ></UpgradeIcon>
+                    <upgrade-icon :class="['av-icon', hoverClass]" ></upgrade-icon>
                 </b-link>
             </div>
         </div>
@@ -28,10 +46,11 @@
 
 <script>
 import UpgradeIcon from '../../assets/images/icons/upgrade.svg';
+import FightIcon from '../../assets/images/icons/fight.svg';
 
 export default {
     name: "Character",
-    components: {UpgradeIcon},
+    components: { UpgradeIcon, FightIcon },
     props: {
         char: {
             type: Object,
@@ -41,6 +60,11 @@ export default {
     data() {
         return {
             hoverClass: ''
+        }
+    },
+    computed: {
+        avatarId() {
+            return 'character-avatar-' + this.char.CharacterId;
         }
     },
     methods: {
