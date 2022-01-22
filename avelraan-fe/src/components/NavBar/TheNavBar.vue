37ff6cbf36e68fc10088096811207ab4b7475289
Maryna Iholnykova <maryna.iholnykova@ionos.com>
2022-01-22 16:24:33 +0200

    collapse menu in the NavBar

--- avelraan-fe/src/components/NavBar/TheNavBar.vue
+++ avelraan-fe/src/components/NavBar/TheNavBar.vue
@@ -1,21 +1,36 @@
 <template>
 <div class="py-3" id="the_nav_bar">
-    <b-nav align="center">
-        <b-nav-item v-for="item in navItems"
-                    :key="item.name + '-nav-item'"
-                    :to="item.path"
-                    :active="isNavItemActive(item.name)"
-                    class="mb-1"
-        >
-            {{ item.name }}
-        </b-nav-item>
-    </b-nav>
+    <b-navbar toggleable="sm" type="dark">
+        <b-navbar-toggle target="nav-collapse" class="border-0">
+            <template>
+                <menu-icon class="menu-icon"></menu-icon>
+            </template>
+        </b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="mx-auto">
+                <b-nav-item v-for="item in navItems"
+                            :key="item.name + '-nav-item'"
+                            :to="item.path"
+                            :active="isNavItemActive(item.name)"
+                            class="mb-1 text-center"
+                            style="min-width: 4rem"
+                >
+                    {{ item.name }}
+                </b-nav-item>
+            </b-navbar-nav>
+
+        </b-collapse>
+
+    </b-navbar>
 </div>
 </template>
 
 <script>
+import MenuIcon from '../../assets/images/icons/hamburger-menu.svg';
+
 export default {
     name: "TheNavBar",
+    components: { MenuIcon },
     computed: {
         navItems() {
             return this.$router.options.routes.find(route => (route.name === 'Base')).children;
